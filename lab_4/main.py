6d37d5df20bf1fa7a74466decf73c76a6b6b4cf2
DaniinXorchenabo <rkbcu@mail.ru>
2021-12-01 20:46:03 +0300

    добавил обработку бинарных операций, добавил подоперации и константы

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,10 +1,14 @@
 import re
 import regex
 
-UNARY_OPERATION = r"NOT(?![A-Za-z])"
+UNARY_OPERATION = r"\.NOT\."  # \.NOT\.(?![A-Za-z])
+CONSTANTS = r"(?:0|1)"
+BINARY_OPERATIONS = r"\.AND\.|\.OR\.|\.IMP\."
 
 # https://dev-gang.ru/article/rekursivnye-reguljarnye-vyrazhenija-v-python-wz6w1co7jp/
-BRACKETS_PARSE = lambda i: fr"(\((?:(?<=[()])[^()]*(?=[()]))*(?:(?{i})*(?:(?<=[()])[^()]*(?=[()])))*(?:(?<=[()])[^()]*(?=[()]))*\))"
+BRACKETS_PARSE = lambda \
+        i: fr"(\((?:(?<=[()])[^()]*(?=[()]))*(?:(?{i})*(?:(?<=[()])[^()]*(?=[()])))*(?:(?<=[()])[^()]*(?=[()]))*\))"
+
 
 class BaseItem(object):
     key = r".*"
@@ -95,7 +99,7 @@ class OneAssignment(BaseItem):
 print(OneAssignment(" AAD = 4 5 ;  SDF=2345; DSF =345; SDF= 235; ASFS = 2354 ; "))
 
 
-class OneExpression(BaseItem):
+class OneUnaryExpression(BaseItem):
     # ((?:NOT(?![A-Za-z])|-))(\s*\([^)]+\)\s*?|\s*[A-Za-z]+\s*?|\s*[01]\s*?)
     #
     # ((?:{UNARY_OPERATION})?)(\s*\([^)]+\)\s*|\s+[A-Za-z]+\s*|\s+[01]\s*)
@@ -107,7 +111,7 @@ class OneExpression(BaseItem):
     # (\(((?<=[()])[^()]*(?=[()]))*((?0)*((?<=[()])[^()]*(?=[()])))*((?<=[()])[^()]*(?=[()]))*\))
 
     # ((?:{UNARY_OPERATION})?)(\s*{BRACKETS_PARSE(2)}\s*?|\s*[A-Za-z]+\s*?|\s*[01]\s*?)
-    key = fr"(\s*(?:{UNARY_OPERATION})?)(\s*{BRACKETS_PARSE(2)}\s*?|\s*[A-Za-z]+\s*?|\s*[01]\s*?)"
+    key = fr"(\s*(?:{UNARY_OPERATION})?)(\s*{BRACKETS_PARSE(2)}\s*?|\s*[A-Za-z]+\s*?|\s*{CONSTANTS}\s*?)"
 
     def __new__(cls, data) -> list:
         print(cls.key)
@@ -117,7 +121,80 @@ class OneExpression(BaseItem):
         return [data[1], data[2]]
 
 
-print(OneExpression(" NOT(ASR OR 0 AND (1 OR (HELP AND NOT(1 AND 1))) (())) () "))
+print(OneUnaryExpression(" NOT(ASR OR 0 AND (1 OR (HELP AND NOT(1 AND 1))) (())) () "))
+
+
+class OneBinaryExpression(BaseItem):
+    # ((?:NOT(?![A-Za-z])|-))(\s*\([^)]+\)\s*?|\s*[A-Za-z]+\s*?|\s*[01]\s*?)
+    #
+    # ((?:{UNARY_OPERATION})?)(\s*\([^)]+\)\s*|\s+[A-Za-z]+\s*|\s+[01]\s*)
+    #
+    # ((\(([^()]*?)*?(?0)*?([^()]*?)*?\)([^()]*?)*?)([^()]*)*(?0)*?)
+    # ((\(([^()]*?(?=[()]))*?(?0)*?([^()]*?(?=[()]))*?\)([^()]*?(?=[()]))*?)([^()]*(?=[()]))*(?0)*?)
+
+    # (.*?)(\([^()]*(?=[()])(?<=[()])(?1)     \))
+    # (\(((?<=[()])[^()]*(?=[()]))*((?0)*((?<=[()])[^()]*(?=[()])))*((?<=[()])[^()]*(?=[()]))*\))
+
+    # ((?:{UNARY_OPERATION})?)(\s*{BRACKETS_PARSE(2)}\s*?|\s*[A-Za-z]+\s*?|\s*[01]\s*?)
+    key = fr"((\s*{BRACKETS_PARSE(3)}\s*?|\s*[A-Za-z]+\s*?|\s*{CONSTANTS}\s*?)(?:\s*({BINARY_OPERATIONS}))(\s*{BRACKETS_PARSE(6)}\s*?|\s*[A-Za-z]+\s*?|\s*{CONSTANTS}\s*?))"
+
+    def __new__(cls, data) -> list:
+        # print(cls.key)
+        assert (data := regex.search(cls.key, data))
+        # print(data)
+        # print(data[0], "|", data[1], "|", data[2], "|", data[3], "|",  data[4], "|",  data[5], "|",  data[6])
+        return [data[3], data[4], data[6]]
+
+
+print(OneBinaryExpression(" ((d(d(d(df)f(dfg)f(g))f)d(d)d(d)d(d)f)) .AND. (---(d(d(d( df)f(dfg)f(g))f)d(d)d(d)d(d)f))   "))
+
+
+class UnaryOperation(BaseItem):
+    key = fr"^\s*({UNARY_OPERATION})\s*$"
+
+    def __new__(cls, data) -> list:
+        assert (data := re.search(cls.key, data))
+        print(data[0], "|", data[1])
+        return [data[1]]
+
+
+print(UnaryOperation(" .NOT. "))
+
+
+class BinaryOperation(BaseItem):
+    key = fr"^\s*({BINARY_OPERATIONS})\s*$"
+
+    def __new__(cls, data) -> list:
+        assert (data := re.search(cls.key, data))
+        print(data[0], "|", data[1])
+        return [data[1]]
+
+
+print(BinaryOperation(" .AND. "))
+
+
+class SubExpression(BaseItem):
+    key = fr"\s*{BRACKETS_PARSE(0)}\s*"
+
+    def __new__(cls, data) -> list:
+        assert (data := regex.search(cls.key, data))
+        print(data[0], "|", data[1])
+        return [str(data[1])[1:-1]]
+
+
+print(SubExpression(" (DFG()(())) "))
+
+
+class ReadConst(BaseItem):
+    key = fr"\s*({CONSTANTS})\s*"
+
+    def __new__(cls, data) -> list:
+        assert (data := regex.search(cls.key, data))
+        print(data[0], "|", data[1])
+        return [data[1]]
+
+
+print(ReadConst(" 1 "))
 
 GRAPH = {
     Program: [InitializingVariables, ProgramBody],
@@ -126,6 +203,11 @@ GRAPH = {
     ReadWord: [],
     ProgramBody: [AssignmentList],
     AssignmentList: [OneAssignment, AssignmentList],
-    OneAssignment: [ReadWord, OneExpression],
-    OneExpression: [],
+    OneAssignment: [ReadWord, OneUnaryExpression, OneBinaryExpression],
+    OneUnaryExpression: [UnaryOperation, SubExpression],
+    OneBinaryExpression: [SubExpression, BinaryOperation],
+    UnaryOperation: [],
+    BinaryOperation: [],
+    SubExpression: [OneUnaryExpression, ReadWord, ReadConst],
+    ReadConst: [],
 }
